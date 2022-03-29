dae555feaf409ac3823f53738165d476b8c68261
punithkamini <78608978+punithkamini@users.noreply.github.com>
2022-03-29 23:27:58 +0530

    Update example25.py

--- example25.py
+++ example25.py
@@ -2,6 +2,7 @@ l=[]
 l1=[]
 d=[]
 d1=[]
+
 def solution(A):
     for i in range(1,len(A)):
         if i==1:
@@ -11,21 +12,29 @@ def solution(A):
             elif A[i]>A[i-1] or A[i]==A[i-1]:
                 l1.extend([A[i-1],A[i]])
                 d1.extend([A[i-1], A[i]])
-            #print(l)
         else:
-            #print(A[i])
             if len(l)!=0:
+                tp=0
                 if (A[i]>d[-1] and d[-1]<d[-2]) or (A[i]<d[-1] and d[-1]>d[-2]):
                     l.append(A[i])
                     d.append(A[i])
                 elif (A[i]>d[-1] and d[-1]>d[-2]) or (A[i]<d[-1] and d[-1]<d[-2]):
-                    d.append(A[i])
+                    l.append(A[i])
+                    if A[i] != tp and A[i] > d[-1] and A[i] > d[-2] and i == len(A) - 1:
+                        d.append(A[i])
+                        tp = A[i - 1]
+
             if len(l1)!=0:
+                tp=0
                 if (A[i]>d1[-1] and d1[-1]<d1[-2]) or (A[i]<d1[-1] and d1[-1]>d1[-2]):
                     l1.append(A[i])
                     d1.append(A[i])
                 elif (A[i]>d1[-1] and d1[-1]>d1[-2]) or (A[i]<d1[-1] and d1[-1]<d1[-2]):
-                    d1.append(A[i])
+                    l1.append(A[i])
+                    if A[i]!=tp and A[i]>d1[-1] and A[i] > d1[-2] and i==len(A)-1:
+                        d1.append(A[i])
+                        tp = A[i-1]
+
                 elif (A[i] == d1[-1] and d1[-1] < d1[-2]) or (A[i] == d1[-1] and d1[-1] > d1[-2]) or (A[i] == d1[-1] and d1[-1] == d1[-2]):
                     d1.append(A[i])
                 elif (A[i] > d1[-1] and d1[-1] == d1[-2]) or (A[i] < d1[-1] and d1[-1] == d1[-2]):
@@ -33,16 +42,14 @@ def solution(A):
                     l1.append(A[i])
                     l1.pop(1)
 
-    if len(l)!=0:
+    if len(d)>len(l):
         print(len(d)-len(l))
-    elif len(l1) != 0:
+    elif len(d) < len(l):
+        print(len(l)-len(d))
+    elif len(d1)>len(l1):
         print(len(d1)-len(l1))
+    elif len(d1)<len(l1):
+        print(len(l1)-len(d1))
 
-solution(A5)
 
-A=[5,3,5,7,5]
-A1=[5,2,6,4,7,3]
-A2=[2,5,4,6,3,7]
-A3=[1,3,1,2]
-A4=[5,5,3,4,5]
-A5=[5,5,6,4,5]
+solution(A)
